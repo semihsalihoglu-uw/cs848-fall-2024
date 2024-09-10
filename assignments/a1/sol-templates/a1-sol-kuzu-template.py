ac76eb738136f469ca5794772c063641f313cbd3
Semih Salihoglu <ssalihoglu@uwaterloo.ca>
2024-09-10 15:27:59 -0400

    Update a1-sol-kuzu-template.py

--- assignments/a1/sol-templates/a1-sol-kuzu-template.py
+++ assignments/a1/sol-templates/a1-sol-kuzu-template.py
@@ -4,19 +4,42 @@ import shutil
 def q1(conn: kz.Connection) -> kz.QueryResult:
   return conn.execute(
       """                                                                                                                          
-      <Put your query here>                                                                                                          
+      <Write Your Query Here>                                                                                                          
       """)
 
+def q2(conn: kz.Connection) -> kz.QueryResult:
+  return conn.execute(
+      """                                                                                                                          
+      <Write Your Query Here>                                                                                                          
+      """)
+
+def q3(conn: kz.Connection) -> kz.QueryResult:
+  return conn.execute(
+      """                                                                                                                          
+      <Write Your Query Here>                                                                                                          
+      """)
+
+def q4(conn: kz.Connection) -> kz.QueryResult:
+  return conn.execute(
+      """                                                                                                                          
+      <Write Your Query Here>                                                                                                          
+      """)
+
+def q5(conn: kz.Connection) -> kz.QueryResult:
+  return conn.execute(
+      """                                                                                                                          
+      <Write Your Query Here>                                                                                                          
+      """)
 
 def main():
     shutil.rmtree("./ex_db_kuzu", ignore_errors=True)
     db = kz.Database("./ex_db_kuzu")
     conn = kz.Connection(db)
     res = q1(conn)
-    // res = q2(conn)
-    // res = q3(conn)
-    // res = q4(conn)
-    // res = q5(conn)
+    # res = q2(conn)
+    # res = q3(conn)
+    # res = q4(conn)
+    # res = q5(conn)
     while res.has_next():
       print(res.get_next())
 
